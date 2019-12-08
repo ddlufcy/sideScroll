8f915b8bd2002cdb70f9991a8c17ec6216f82273
Daniel Lufcy <ddlufcy@gmail.com>
2019-12-07 23:20:59 -0500

    home title

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//side scrolling
 var item = document.getElementsByTagName('MAIN')[0];
 
 window.addEventListener('wheel', function(e) {
@@ -5,3 +6,8 @@ window.addEventListener('wheel', function(e) {
   if (e.deltaY > 0) item.scrollLeft += 100;
   else item.scrollLeft -= 100;
 });
+
+// // home squares
+// let squares = document.getElementByClassName('square');
+// let sq = [squares, squares];
+
